33d027b29bd7a30cf7a4b9780bb93876e1b0b7db
Facundo Dominguez <facundoariel.dominguez@gmail.com>
2017-10-10 22:09:40 -0300

    Add files via upload

---
+++ punteros_parte2/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funcpunteros.h"
+
+
+int main()
+{
+    char nombre[51];
+    int *ptrPer;
+
+    ptrPer=&per;
+
+    printf("ingrese dni: ")
+
+    ptrPer->dni;
+    ptrPer->nombre;
+    ptrPer->FechaNac.dia;
+    ptrPer->FechaNac.mes;
+    ptrPer->FechaNac.anio;
+
+
+    return 0;
+
+
+}
+
